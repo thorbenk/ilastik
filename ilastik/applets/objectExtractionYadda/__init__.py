5fbb3fd7fd2ea9c83969d44fa2fd5eaa0e40e41f
Kemal Eren <kemal.eren@iwr.uni-heidelberg.de>
2013-01-29 17:23:16 +0100

    removed ObjectExtractionYadda

--- ilastik/applets/objectExtractionYadda/__init__.py
+++
@@ -1,1 +0,0 @@
-from objectExtractionApplet import ObjectExtractionApplet
--- ilastik/applets/objectExtractionYadda/objectExtractionSerializer.py
+++
@@ -1,8 +0,0 @@
-from ilastik.applets.base.appletSerializer import AppletSerializer
-from lazyflow.rtype import List
-
-class ObjectExtractionSerializer(AppletSerializer):
-    def __init__(self, projectFileGroupName, operator):
-        slots = []
-        super(ObjectExtractionSerializer, self).__init__(
-            projectFileGroupName, slots, operator=operator)
