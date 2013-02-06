eff1d56a3b44acc75f4c44655e5ec502346163ad
Kemal Eren <kemal.eren@iwr.uni-heidelberg.de>
2013-02-06 11:57:32 +0100

    got SerialDictSlot working

--- ilastik/applets/objectClassification/objectClassificationSerializer.py
+++ ilastik/applets/objectClassification/objectClassificationSerializer.py
@@ -1,4 +1,5 @@
-from ilastik.applets.base.appletSerializer import AppletSerializer, SerialSlot
+from ilastik.applets.base.appletSerializer import \
+  AppletSerializer, SerialSlot, SerialDictSlot
 
 
 class Groups(object):
@@ -6,10 +7,8 @@ class Groups(object):
 
 
 class ObjectClassificationSerializer(AppletSerializer):
-    """
-    """
     def __init__(self, topGroupName, operator):
-        serialSlots = []
+        serialSlots = [SerialDictSlot(operator.LabelInputs, transform=int)]
         super(ObjectClassificationSerializer, self ).__init__(topGroupName,
                                                               slots=serialSlots,
                                                               operator=operator)
